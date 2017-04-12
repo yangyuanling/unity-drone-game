1341065a04d3c5854eb167f14f315ac9cb8cc946
Juan Vaca <juanjvaca@Juans-MacBook.local>
2017-04-12 14:59:11 -0500

    FINALLY GOT THE COLLIDERS TO WORK
    I had to add a collider to every piece of the drone in order to work

---
+++ Assets/Scripts/OnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision collision) 
+	{
+		Debug.Log ("Enter called.");
+	}
+
+	void OnCollisionStay (Collision collision)
+	{
+		Debug.Log ("Stay occuring..");
+	}
+
+	void OnCollisionExit (Collision collision)
+	{
+		Debug.Log ("Exit called..");
+	}
+}
