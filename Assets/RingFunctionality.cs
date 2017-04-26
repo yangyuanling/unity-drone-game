bfe1b7cd5acd8b47c0594ab2b0d75ab179de85c0
Juan Vaca <juanjvaca@Juans-MacBook.local>
2017-04-25 19:30:04 -0500

    Merge branch 'master' into first-branch
    # Conflicts:
    #	Assets/Arabic city/Scene/Arabic_city_sample_01.unity

---
+++ Assets/RingFunctionality.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingFunctionality : MonoBehaviour {
+
+    public GameObject currentRing;
+    public GameObject nextRing;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Entered");
+
+        if (col.gameObject.tag == "Player")
+        {
+            nextRing.gameObject.SetActive(true);
+            currentRing.gameObject.SetActive(false);
+        }
+    }
+
+        // Use this for initialization
+        void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
